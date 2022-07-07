41386a153d9ca9797538299f341f89dfd6b9faa5
FilipKufalov <filip.kufalov@hotmail.com>
2022-07-08 01:08:35 +0200

    first program

---
+++ PresentDataFromFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PresentDataFromFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: " + fileName + " file.txt");
+            } else
+            {
+                string[] lines = System.IO.File.ReadAllLines(@System.AppContext.BaseDirectory + args[0]);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Console.WriteLine(lines[i]);
+                }
+            }
+        }
+    }
+}
