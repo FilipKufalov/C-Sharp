7c0bcf489d84a062a94ef906096c683f8086cdd0
FilipKufalov <filip.kufalov@hotmail.com>
2022-08-26 18:58:15 +0200

    added HTTPWebChecker & ArgsPractice program

---
+++ ArgsPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArgsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+
+        }
+    }
+}
---
+++ HTTPWebChecker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HTTPWebChecker
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string fileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: " + fileName + " file.txt");
+            }
+            else
+            {
+                string[] lines = System.IO.File.ReadAllLines(@System.AppContext.BaseDirectory + args[0]);
+                foreach (string line in lines)
+                {
+                    using var client = new System.Net.Http.HttpClient();
+                    try
+                    {
+                        var result = await client.GetAsync("https://" + line);
+                        if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            Console.WriteLine(line + " is UP!");
+                            using StreamWriter file = new("alive.txt", append: true);
+                            await file.WriteLineAsync(line);
+                        }
+                        else
+                        {
+                            Console.WriteLine(line + " Unknown Status!");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(line + " is DOWN!");
+                    }
+                    finally
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
