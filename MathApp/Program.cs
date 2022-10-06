304a7c4a24e1b3b1e54576bac26fcfddbb1f8b4e
FilipKufalov <filip.kufalov@hotmail.com>
2022-10-06 20:00:59 +0200

    math functions calling

---
+++ MathApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using MathLib;
+
+//Console.WriteLine($"{Math2.Add(1,4)}");
+Console.WriteLine(Math2.Add(2,4));
+Console.WriteLine(Math2.Subtract(5,10));
+Console.WriteLine(Math2.Divide(3,4));
+Console.WriteLine(Math2.Multiply(3,4));
+Console.WriteLine(Math2.Modulo(3,6));
---
+++ MathLib/Math.cs
@@ -0,0 +1,12 @@
+﻿namespace MathLib
+{
+    public class Math2
+    {
+        public Math2() { return; }
+        public static int Add(int a, int b) { return a + b; }
+        public static int Subtract(int a, int b) { return a - b; }
+        public static int Multiply(int a, int b) { return a * b; } 
+        public static int Divide(int a, int b) { return a / b; }
+        public static int Modulo(int a, int b) { return a * b; }
+    }
+}
